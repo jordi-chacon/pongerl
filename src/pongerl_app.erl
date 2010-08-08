8d02c690b3d89971897058e3e2aa9f448ccc3c76
Jordi Chacon <jordi.chacon@gmail.com>
2010-08-08 18:13:29 +0200

    Supervisor and application modules created. App file created

---
+++ src/pongerl_app.erl
@@ -0,0 +1,17 @@
+%% @author Jordi Chacon <jordi.chacon@gmail.com>
+%% @copyright 2010 Jordi Chacon
+
+%% @doc Callbacks for the pongerl application.
+
+-module(pongerl_app).
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+-include("pongerl.hrl").
+
+start(_, _) ->
+    polish_sup:start_link().
+
+stop(_) ->
+    ok.
