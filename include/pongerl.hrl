cce83168d6bfdc1ec12ef694ba10c0d128bf7c79
Jordi Chacon <jordi.chacon@gmail.com>
2010-08-14 13:28:10 +0200

    Implemented basic funcionality of the pongerl main server

---
+++ include/pongerl.hrl
@@ -0,0 +1,3 @@
+% A client can move up and down
+-define(UP, 1).
+-define(DOWN, 2).
--- src/pongerl_server.erl
+++ src/pongerl_server.erl
@@ -6,7 +6,9 @@
 -behaviour(gen_server).
 
 %% API
--export([]).
+-export([connect_client/0,
+	 change_client_position/2,
+	 get_state/0]).
 
 %% gen_server callbacks
 -export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -14,111 +16,64 @@
 
 -define(SERVER, ?MODULE). 
 
--record(state, {}).
+-record(state, {clients = [], last_id = 0}).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
 
-    
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initiates the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([]) ->
     {ok, #state{}}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+connect_client() ->
+    gen_server:call(?MODULE, {connect_client}).
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
+get_state() ->
+    gen_server:call(?MODULE, {get_state}).
+
+change_client_position(ClientID, Direction) ->
+    gen_server:call(?MODULE, {change_client_position, ClientID, Direction}).
+
+handle_call({connect_client}, _From, State) ->
+    do_connect_client(State);
+handle_call({get_state}, _From, State) ->
+    do_get_state(State);
+handle_call({change_client_position, ClientID, Direction}, _From, State) ->
+    do_change_client_position(ClientID, Direction, State);
+handle_call(_Request, _From, State) ->
+    {reply, error, State}.
+
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_info(_Info, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+do_connect_client(State) ->
+    ID = State#state.last_id,
+    Clients = [Id|State#state.clients],
+    case Clients of
+	[ID1, ID2] -> pongerl_engine:start(ID1, ID2),
+	_          -> ok
+    end.
+    NewState = State#state{last_id = ID+ 1, clients = Clients},
+    {reply, Id, NewState}.
+    
+do_get_state(State) ->
+    Reply = pongerl_engine:get_state(),
+    {reply, Reply, State}.
+
+do_change_client_position(ClientID, Direction, State) ->
+    game_engine:change_client_position(ClientID, Direction),
+    {reply, ok, State}.
