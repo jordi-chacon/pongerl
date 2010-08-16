0131f3b63d902e7a8fa0f15b04ba38e06db9be75
Jordi Chacon <jordi.chacon@gmail.com>
2010-08-16 23:25:10 +0200

    Improved start script to quickly and easily start server, client1 and client2 from it

--- start.sh
+++ start.sh
@@ -3,8 +3,29 @@ cd `dirname $0`
 
 . ./dep.inc
 
-echo "Starting Pongerl..."
-erl \
-    -sname ${NAME} \
-    -pa ./ebin ${CECHO_EBIN} \
-    -eval "application:start(pongerl)"
+execute_server() {
+    erl -sname server -pa ./ebin ${CECHO_EBIN} -eval "application:start(pongerl)" -setcookie pongerl
+}
+
+execute_client() {
+    erl -pa ./ebin ${CECHO_EBIN} -sname $1 -setcookie pongerl -eval 'pongerl_client:start()' +A 200
+}
+
+
+if [ $# -eq 0 ]; then
+    execute_server
+else
+    opt=$(echo $1 | sed 's/=.*//')
+    case $opt in
+	"--server")
+	    execute_server
+	    ;;
+	"--c1")
+	    execute_client "c1"
+	    ;;
+	"--c2")
+	    execute_client "c2"
+	    ;;
+    esac
+fi
+
